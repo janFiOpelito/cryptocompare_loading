4f9e29f2a2768c1d8c2e4232817e2007138b5fe7
janFiOpelito <jphdsn@gmail.com>
2020-03-27 14:09:22 +0100

    added code for feature

--- cryptocompare_loading/cryptocompare_hours.py
+++ cryptocompare_loading/cryptocompare_hours.py
@@ -15,14 +15,57 @@ import ccxt
 from datetime import timedelta
 from pytz import timezone
 import pytz
+import os
+#import boto as aws
+from pathlib import Path
 
 utc = pytz.utc
 from datetime import datetime
 from datetime import timezone
-dt = datetime(2019, 4, 19)
+dt = datetime(2020, 3, 25)
 timestamp = dt.replace(tzinfo=timezone.utc).timestamp()
 print(int(timestamp))
 import datetime
+
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+s3 = boto3.client('s3',aws_access_key_id= AWS_ACCESS_KEY_ID,aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+response = s3.list_buckets()
+
+bucket_name=''
+# Output the bucket names
+print('Existing buckets:')
+for bucket in response['Buckets']:
+    bucket_name=bucket["Name"]
+    print(f'  {bucket["Name"]}')
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3_client = s3
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
 
 def daily_price_historical(symbol, comparison_symbol, limit, aggregate, toTs='', exchange='' ):
 
@@ -50,6 +93,7 @@ def add_columns_sort (df, key, crypto_base, crypto_quote):
         df=df.sort_values(by=['time'], ascending=False)
     return df
 
+"""
 def create_table_to_postgresql(tablename, df):
 
     if not sql_db.has_table(tablename) and not df.empty:
@@ -63,6 +107,7 @@ def create_table_to_postgresql(tablename, df):
 
         sql_table = pd.io.sql.SQLTable(*args, **kwargs)
         sql_table.create()
+"""
 
 time_delta=1
 ohlcv_all = []#pd.DataFrame()
@@ -75,10 +120,10 @@ nb_lignes = 47 # une année, 365 jours (0-364)
 
 for exchange in exchanges:
 
-    engine = create_engine('postgresql+psycopg2://postgres:password@localhost/'+exchange+'_dev')
+    #engine = create_engine('postgresql+psycopg2://postgres:password@localhost/'+exchange+'_dev')
 
-    conn = engine.connect()
-    sql_db = pd.io.sql.SQLDatabase(engine)
+    #conn = engine.connect()
+    #sql_db = pd.io.sql.SQLDatabase(engine)
 
     if exchange=='bitfinex':
         exchange_ccxt = ccxt.bitfinex()
@@ -104,6 +149,10 @@ for exchange in exchanges:
     first_time = int(dt.replace(tzinfo=timezone.utc).timestamp())
     print ("first_time",first_time)
 
+    data_folder = Path("../out/data")
+    filename='days_' + exchange + '_'+ '2015'+'_2020'
+    #filename=data_folder/filename
+
     for symbol in exchange_ccxt_markets.keys():
 
         crypto_base, crypto_quote=re.split("[/]", symbol)
@@ -116,12 +165,19 @@ for exchange in exchanges:
         if not ohlcv.empty:
             print('###close timestamp debut###', ohlcv.iloc[-1]['close'],ohlcv.iloc[-1]['timestamp'])
             print('###close timestamp fin###',ohlcv.iloc[0]['close'], ohlcv.iloc[0]['timestamp'])
+            with open(filename, 'a') as f:
+                ohlcv.to_csv(f, header=f.tell()==0)
 
-        table_name = 'days_' + exchange + '_'+ '2015'+'_2019'
-        create_table_to_postgresql (table_name, ohlcv)
+    upload_file(filename, bucket_name, object_name=None)
+        #table_name = 'days_' + exchange + '_'+ '2015'+'_2019'
+        #create_table_to_postgresql (table_name, ohlcv)
 
-        if not ohlcv.empty and sql_db.has_table(table_name):
+        #if not ohlcv.empty and sql_db.has_table(table_name):
 
-            ohlcv.to_sql(table_name, conn, if_exists='append', index=False)
+            #ohlcv.to_sql(table_name, conn, if_exists='append', index=False)
 
     #time.sleep(2000)
+
+
+#with open("FILE_NAME", "rb") as f:
+#    s3.upload_fileobj(filename, "BUCKET_NAME", "OBJECT_NAME")
